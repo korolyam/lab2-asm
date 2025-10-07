8de32a6a80c8f10724118db70a6cd06bd24b12f9
Администратор <@314-10>
2025-10-07 12:53:54 +0300

    Добавьте файлы проекта.

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main()
+{
+    int z, b;
+    std::cout << "Vvedite argument radian z\n";
+    std::cin >> z;
+    __asm
+    {
+        fld1
+        fadd st(0), st(0)
+        fld z
+        fdiv st(0), st(1)
+        fptan
+        fxch st(1)
+        fmul st(0), st(0)
+        fadd st(0), st(1)
+        fmul st(0), st(0)
+        fld z
+        fsincos
+        fmul st(0), st(1)
+        fmul st(0), st(2)
+        fstp b
+    }
+    std::cout << "b = " << b << "\n";
+}
+
