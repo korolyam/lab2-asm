14008df4add3eb72e48436f7a1e05d461c84a729
Korolyam <worldofcrew1@gmail.com>
2025-10-27 16:09:40 +0200

    done

---
+++ 2.1.2/2.1.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+int main()
+{
+    double x;
+    double q = 0;
+    std::cout << "Vvedite argument  x\n";
+    std::cin >> x;
+    __asm
+    {
+        fld1
+        fadd st(0), st(0)
+        fld x
+        fptan
+        fxch st(1)
+        fmul st(0), st(2)
+        fst st(1)
+        fyl2x
+        fld1
+        fld x
+        fmul st(0), st(0)
+        fld x
+        fmul st(0), st(1)
+        fadd st(0), st(2)
+        fsqrt
+        fcos
+        fmul st(0), st(1)
+        f2xm1
+        fadd st(0), st(2)
+        fdiv st(0), st(3)
+        fld x
+        fsub st(0), st(2)
+        fcos
+        fmul st(0), st(0)
+        fsubr st(0), st(1)
+        fstp q
+    }
+    std::cout << "q = " << q << "\n";
+}
--- 2.1/2.1.cpp
+++ 2.1/2.1.cpp
@@ -1,18 +1,21 @@
 ﻿#include <iostream>
 int main()
 {
-    int z, b;
+    double z;
+    double b = 0;
     std::cout << "Vvedite argument radian z\n";
     std::cin >> z;
     __asm
     {
+        fld z
         fld1
         fadd st(0), st(0)
-        fld z
-        fdiv st(0), st(1)
+        fdiv st(1), st(0)
+        fxch
         fptan
-        fxch st(1)
+        fstp st(0)
         fmul st(0), st(0)
+        fld1
         fadd st(0), st(1)
         fmul st(0), st(0)
         fld z
@@ -20,6 +23,7 @@ int main()
         fmul st(0), st(1)
         fmul st(0), st(2)
         fstp b
+
     }
     std::cout << "b = " << b << "\n";
 }
